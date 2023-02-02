9b0d887c7b6d497f01c445a0d64414ab3cf77928
Maciek Sobiecki <sobiecki.maciek@gmail.com>
2023-02-02 13:58:48 +0000

    Updated the script file from bash to sh and added permissions

--- script.sh
+++ script.sh
@@ -5,11 +5,15 @@ cd fol_1
 touch 1_1.txt
 touch 1_2.txt
 touch 1_3.txt
+chmod 600 1_1.txt 1_3.txt
+chmod 700 1_2.txt 
 cd ..
 cd fol_2
 touch 2_1.txt
 touch 2_2.txt
 touch 2_3.txt
+chmod 600 2_1.txt 2_3.txt
+chmod 700 1_2.txt 
 cd ..
 touch .gitignore.txt
 echo "fol_1" >> .gitignore.txt
